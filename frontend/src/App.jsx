23e5f5d1c0c3d5dac1dd260e8fd1d9c33b7a2f2f
DESKTOP-LSLTHMA\Horlescu <adrian.horlescu@gmail.com>
2024-10-05 09:22:39 +0300

    Frontend - fetch cities from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,31 @@ import { Homepage } from "./pages/Homepage";
 import { AppLayout } from "./pages/AppLayout.jsx";
 import { Login } from "./pages/Login.jsx";
 import { CityList } from "./components/CityList.jsx";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000/trips/test";
 
 export function App() {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(function () {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                setCities(data);
+            } catch (error) {
+                alert("There was an error fetching data...");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchCities();
+    }, []);
+    console.log(cities);
+
     return (
         <BrowserRouter>
             <Routes>
@@ -15,8 +38,18 @@ export function App() {
                 <Route path="product" element={<Product />} />
                 <Route path="pricing" element={<Pricing />} />
                 <Route path="app" element={<AppLayout />}>
-                    <Route index element={<CityList />} />
-                    <Route path="cities" element={<CityList />} />
+                    <Route
+                        index
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
+                    />
+                    <Route
+                        path="cities"
+                        element={
+                            <CityList cities={cities} isLoading={isLoading} />
+                        }
+                    />
                     <Route path="countries" element={<p>countires</p>} />
                     <Route path="form" element={<p>form</p>} />
                 </Route>
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,11 +1,5 @@
 import styles from "./Message.module.css";
 
-function Message({ message }) {
-  return (
-    <p className={styles.message}>
-      <span role="img">👋</span> {message}
-    </p>
-  );
+export function Message({ message }) {
+    return <p className={styles.message}>{message}</p>;
 }
-
-export default Message;
