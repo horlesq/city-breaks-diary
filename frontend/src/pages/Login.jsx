5d684d021e0eaddc1574a3376afb85d0b026e9fa
DESKTOP-LSLTHMA\Horlescu <adrian.horlescu@gmail.com>
2024-10-09 11:05:50 +0300

    Frontend - login/register form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
-import styles from "./Login.module.css";
 import { PageNav } from "../components/PageNav";
+import { Button } from "../components/Button";
+import styles from "./Login.module.css";
 
 export function Login() {
-    const [email, setEmail] = useState("jack@example.com");
-    const [password, setPassword] = useState("qwerty");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [isRegistering, setIsRegistering] = useState(false); // State to toggle between login and register forms
 
     return (
         <main className={styles.login}>
@@ -16,6 +19,7 @@ export function Login() {
                         type="email"
                         id="email"
                         onChange={(e) => setEmail(e.target.value)}
+                        required
                         value={email}
                     />
                 </div>
@@ -26,12 +30,38 @@ export function Login() {
                         type="password"
                         id="password"
                         onChange={(e) => setPassword(e.target.value)}
+                        required
                         value={password}
                     />
                 </div>
 
-                <div>
-                    <button>Login</button>
+                {isRegistering && (
+                    <div className={styles.row}>
+                        <label htmlFor="password">Confirm Password</label>
+                        <input
+                            type="password"
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required
+                            value={confirmPassword}
+                        />
+                    </div>
+                )}
+
+                <div className={styles.buttons}>
+                    <Button type="primary">
+                        {isRegistering ? "Register" : "Login"}
+                    </Button>
+                    <button
+                        onClick={(event) => {
+                            event.preventDefault();
+                            setIsRegistering(!isRegistering);
+                        }}
+                        className={styles.toggle}
+                    >
+                        {isRegistering
+                            ? "Already have an account? Login"
+                            : "Don't have an account? Register"}
+                    </button>
                 </div>
             </form>
         </main>
