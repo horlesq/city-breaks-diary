05b86bd84084c11e09207240099a8cbe68a8da8f
horlesq <48680025+horlesq@users.noreply.github.com>
2024-10-14 11:27:35 +0300

    Comments - BackButton.jsx

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -2,13 +2,14 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
 
 export function BackButton() {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); // Hook to access navigation functions
+
     return (
         <Button
-            type="back"
+            type="back" // Styling for the back button
             onClick={(event) => {
-                event.preventDefault();
-                navigate(-1);
+                event.preventDefault(); // Prevent default form submission or link behavior
+                navigate(-1); // Navigate to the previous page in the browser history
             }}
         >
             Back
