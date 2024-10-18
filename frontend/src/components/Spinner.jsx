3ddb83cdca7c3b1071399754fed30fa779fae537
horlesq <48680025+horlesq@users.noreply.github.com>
2024-10-18 09:58:26 +0300

    Delete frontend directory

--- frontend/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Spinner.module.css";
-
-export function Spinner() {
-    return (
-        <div className={styles.spinnerContainer}>
-            <div className={styles.spinner}></div>
-        </div>
-    );
-}
